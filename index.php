ad740eaf490a522b9e4350d5c789b8977c4876e5
Rafael Conrado <rafael070893@hotmail.com>
2022-05-23 20:54:49 -0300

    style: index sessão postagens instagram e  assinar postagens

--- index.php
+++ index.php
@@ -42,12 +42,28 @@ require_once "./src/view/header.php"
         </div>
     </div>
 
+    <!-- Banner e reders sociais  -->
     <div class="container pt-3 ">
         <div class="card mb-3">
             <div class="row g-0">
                 <div class="col-md-8">
                     <div class="card-body">
                         <p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electro</p>
+                    </div>
+                    <div class="row m-2">
+                        <div class="col-1">
+                            <img src="./src/public/images/facebook .png" alt="icone facebook">
+                        </div>
+
+                        <div class="col-1">
+                            <img src="./src/public/images/whatsapp.png" alt="icone facebook">
+
+                        </div>
+
+                        <div class="col-1">
+                            <img src="./src/public/images/instagram.png" alt="icone facebook">
+
+                        </div>
                     </div>
                 </div>
                 <div class="col-md-4">
@@ -121,11 +137,89 @@ require_once "./src/view/header.php"
 
     <div class="container">
         <div class="row">
+            <p class="text-end">Acompanhe nossas instagram.</p>
+        </div>
 
-            <div class="col-3"></div>
+        <div class="row">
+
+            <div class="col">
+                <div class="card" style="width: 25rem;">
+                    <img src="https://img.freepik.com/free-vector/tiny-volunteers-sharing-canned-food-grocery-products-help-support-assistance-from-people-with-charity-cardboard-box-flat-vector-illustration-voluntary-delivery-humanitarian-aid-concept_74855-22077.jpg?w=2000" class="card-img-top" alt="...">
+                </div>
+
+            </div>
+
+            <div class="col">
+
+                <div class="row">
+                    <div class="col imgs-insta">
+                        <div class="card" style="width:8rem;">
+                            <img src="https://img.freepik.com/free-vector/tiny-volunteers-sharing-canned-food-grocery-products-help-support-assistance-from-people-with-charity-cardboard-box-flat-vector-illustration-voluntary-delivery-humanitarian-aid-concept_74855-22077.jpg?w=2000" class="card-img-top" alt="...">
+                        </div>
+                    </div>
+                    <div class="col imgs-insta">
+                        <div class="card" style="width:8rem;">
+                            <img src="https://img.freepik.com/free-vector/tiny-volunteers-sharing-canned-food-grocery-products-help-support-assistance-from-people-with-charity-cardboard-box-flat-vector-illustration-voluntary-delivery-humanitarian-aid-concept_74855-22077.jpg?w=2000" class="card-img-top" alt="...">
+                        </div>
+                    </div>
+                    <div class="col imgs-insta">
+                        <div class="card" style="width:8rem;">
+                            <img src="https://img.freepik.com/free-vector/tiny-volunteers-sharing-canned-food-grocery-products-help-support-assistance-from-people-with-charity-cardboard-box-flat-vector-illustration-voluntary-delivery-humanitarian-aid-concept_74855-22077.jpg?w=2000" class="card-img-top" alt="...">
+                        </div>
+                    </div>
+                    <div class="col imgs-insta">
+                        <div class="card" style="width:8rem;">
+                            <img src="https://img.freepik.com/free-vector/tiny-volunteers-sharing-canned-food-grocery-products-help-support-assistance-from-people-with-charity-cardboard-box-flat-vector-illustration-voluntary-delivery-humanitarian-aid-concept_74855-22077.jpg?w=2000" class="card-img-top" alt="...">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row pt-2">
+                    <div class="col imgs-insta">
+                        <div class="card" style="width:8rem;">
+                            <img src="https://img.freepik.com/free-vector/tiny-volunteers-sharing-canned-food-grocery-products-help-support-assistance-from-people-with-charity-cardboard-box-flat-vector-illustration-voluntary-delivery-humanitarian-aid-concept_74855-22077.jpg?w=2000" class="card-img-top" alt="...">
+                        </div>
+                    </div>
+                    <div class="col imgs-insta">
+                        <div class="card" style="width:8rem;">
+                            <img src="https://img.freepik.com/free-vector/tiny-volunteers-sharing-canned-food-grocery-products-help-support-assistance-from-people-with-charity-cardboard-box-flat-vector-illustration-voluntary-delivery-humanitarian-aid-concept_74855-22077.jpg?w=2000" class="card-img-top" alt="...">
+                        </div>
+                    </div>
+                    <div class="col imgs-insta">
+                        <div class="card" style="width:8rem;">
+                            <img src="https://img.freepik.com/free-vector/tiny-volunteers-sharing-canned-food-grocery-products-help-support-assistance-from-people-with-charity-cardboard-box-flat-vector-illustration-voluntary-delivery-humanitarian-aid-concept_74855-22077.jpg?w=2000" class="card-img-top" alt="...">
+                        </div>
+                    </div>
+                    <div class="col imgs-insta">
+                        <div class="card" style="width:8rem;">
+                            <img src="https://img.freepik.com/free-vector/tiny-volunteers-sharing-canned-food-grocery-products-help-support-assistance-from-people-with-charity-cardboard-box-flat-vector-illustration-voluntary-delivery-humanitarian-aid-concept_74855-22077.jpg?w=2000" class="card-img-top" alt="...">
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+
+
 
         </div>
 
+    </div>
+
+    <!-- Assinar email postagem  -->
+
+    <div class="container pt-3">
+
+        <div class="d-flex justify-content-center">
+            <p class="text-center">Coloque seu melhor email e fique por dentro de tudo! </p>
+        </div>
+
+        <form>
+            <div class="d-flex ">
+                <div class="p-2 flex-grow-1 bd-highlight"><input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></div>
+                <div class="p-2 bd-highlight"> <button type="submit" class="btn btn-primary">Submit</button></div>
+            </div>
+        </form>
     </div>
 
 
