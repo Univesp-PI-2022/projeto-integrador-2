0fc83e4726a1628afe0225af86a771b4e29b87dd
Rafael Conrado <rafael070893@hotmail.com>
2022-05-24 20:05:41 -0300

    style:  footer

--- src/view/footer.php
+++ src/view/footer.php
@@ -1,4 +1,69 @@
 </div>
+<footer style="background-color:#FFE977 ;">
+    <!--Footer  -->
+    <div class="container">
+        <div class="row">
+            <!-- Logo da ong -->
+            <div class="col-5">
+                <div class="row">
+                    <!--Logo  -->
+                    <div class="col">
+                        <div class="card" style="width: 10rem;">
+                            <img src="https://cdn-icons-png.flaticon.com/512/2151/2151295.png" class="card-img-top" alt="...">
+                        </div>
+                    </div>
+                    <!-- Texto -->
+                    <div class="col">
+                        <h3>ong projeto</h3>
+                        <h3>Brasil</h3>
+                    </div>
+                </div>
+            </div>
+            <!--Endereço -->
+            <div class="col">
+                <div class="row">
+                    <p class="text-center">Entre em contato conosco</p>
+                </div>
+                <div class="row">
+                    <p class="text-center">Telefone: 4665-2538</p>
+                </div>
+                <div class="row">
+                    <p class="text-center">Rua Dom José Alves, 251</p>
+                </div>
+                <div class="row">
+                    <p class="text-center">Bairro da paz, São Paulo,SP</p>
+                </div>
+            </div>
+            <!-- Mapa do site -->
+            <div class="col">
+                <div class="row">
+                    <p class="text">Mapa do site</p>
+                </div>
+                <div class="row">
+                    <ul>
+                        <li>Início</li>
+                        <li>Quem somos</li>
+                        <li>Nossos Projetos</li>
+                        <li>Entre em contato</li>
+                        <li>Blog</li>
+                    </ul>
+                </div>
+            </div>
+            <!-- Redes Sociais -->
+            <div class="col pt-3">
+                <ul style="list-style: none;">
+                    <li class="p-2"> <img src="./src/public/images/facebook .png" alt="icone facebook"></li>
+                    <li class="p-2"><img src="./src/public/images/whatsapp.png" alt="icone facebook"></li>
+                    <li class="p-2"><img src="./src/public/images/instagram.png" alt="icone facebook"></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="row">
+            <p>© Nomes - UNIVESP</p>
+        </div>
+    </div>
+</footer>
 </body>
 
 </html>
