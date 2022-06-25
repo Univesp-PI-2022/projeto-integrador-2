a395127e55baafbb1ba11988b5b8e2ba0826996a
Marcos Lohmann <marcos.lohmann@gmail.com>
2022-06-24 23:01:30 -0300

    Update nossos-projetos.php

--- src/view/nossos-projetos.php
+++ src/view/nossos-projetos.php
@@ -10,9 +10,9 @@ require_once "../view/header.php"
         <h1 class="text-center">Conheça um pouco mais sobre nossos projetos</h1>
 
 
-        <div class="d-flex mt-4">
+        <div class="container mt-4">
             <img width="300rem" height="300rem" src="../public/images/camp-vakinha.PNG" alt="imagem com qrcode para doações" class='float-start'>
-            <div class="text-start">Projeto Solidário sem FOME</div>
+            <div><h3>Projeto Solidário sem FOME</h3>
             <p>Somos o “Projeto Solidário sem FOME”! Somos um grupo multidisciplinar para INCLUSÃO SOCIAL!<br>
 Atuamos diretamente com moradores em situação de rua há cerca de 20 anos. Estamos na região do Morumbi e atendemos pessoas de toda a cidade de São Paulo.
 Servimos mais de 150 refeições e café da manhã por dia. Nos mantemos através de doações e principalmente pela venda de nossos Pães.<br>
@@ -21,13 +21,14 @@ A demanda aumenta a cada dia!<br>
 Precisamos de novos equipamentos, principalmente uma “amassadeira” (batedeira industrial) e um forno industrial para podermos aumentar a produção de Pães e, consequentemente, atendermos a demanda.nossos Produtos são totalmente Artesanal..
 Para continuarmos nossa MISSÃO precisamos de sua ajuda.<br>
 Doe através de nossa “vaquinha” virtual.<br>
-DOE!!!</p>
+DOE!!!</p></div>
         </div>
 
-        <div class="d-flex mt-4">
+        <div class="container mt-4">
             <img width="300rem" height="300rem" src="../public/images/camp-paosol.PNG" alt="imagem de pães" class='float-end'>
-            <div class="text-start">P&atilde;o Solid&aacute;rio</div>
+            <div><h3>P&atilde;o Solid&aacute;rio</h3>
             <p>Pães Solidarios quem ainda não provou é só encomendar ou levar para sua casa ou seu Condominio e com isso vc estará ajudando nosso Projeto Solidario sem fome. <a href='https://wa.me/5511981923972' target='_blank'>faça sua Doação</a></p>
+                </div>
         </div>
     </div>
 
