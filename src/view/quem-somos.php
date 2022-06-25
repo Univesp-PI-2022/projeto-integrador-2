753e0505a2801e66f66a313259f5bf2da98ca9f5
Marcos Lohmann <marcos.lohmann@gmail.com>
2022-06-24 23:10:53 -0300

    Update quem-somos.php

--- src/view/quem-somos.php
+++ src/view/quem-somos.php
@@ -9,7 +9,7 @@ include_once("header.php");
         <h1 class="text-center">A ong Projeto Brasil</h1>
 
         <div class="d-flex">
-            <img width="200rem" height="200rem" src="https://s3-alpha-sig.figma.com/img/5657/eb72/eaec40b1f437319784d6df5726174300?Expires=1655683200&Signature=GMiewoxM2f5s1dUl0NPgFydEXp5PBL4DLuqjlKxHD8AxDECq21M6Y2N7GapT4U-SNbldTBDgWprT4UrQAjDas3KqNH0S1kECiNgAvUI2jawILYJ6jXiCf9vHu9drC-9ZkamBh4yVuMU9UGAyDGnFGbTdRAUeua-xMu02eMP5IXmd97f1AneKhvr2mUk-w-udln6BuPBf72fJLDcHzv4x4cFAX~9ok~hD2eOAkw2jxoGsOIY0E6fRuh6MNbP5WflYW1jYLlPD8-NX3DnuNmneBD6gZIm3QJYfRMFtwgWmeSTFhS-JOGD28BFQt~DmV4sfP7ZRTDG8WIl6q21KQKEGiw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" alt="">
+            <img width="300rem" height="300rem" src="../public/images/main-theme.PNG" alt="vamos ajudar a quem precisa e tem fome">
             <p class="ms-3">Grupo Multidisciplinar para Inclusão Social (Grupo Brasil), hoje com o Projeto Praça Solidaria para atender prontamente a pandemia do Covid-19 em caráter emergencial atender os moradores de rua e pessoas em condições de fragilidade social e vulnerabilidade. Somos uma entidade pessoa jurídica de direito privado e sem fins lucrativos que é regido pelo Estatuto (registrado no 3º Oficial de Registro de Títulos e Documentos e Civil de Pessoas Jurídicas de São Paulo, microfilme nº 458570) e pela legislação em vigor. No desenvolvimento das nossas atividade a entidade observará os princípios da legalidade, impessoalidade, moralidade, publicidade, economicidade e da eficiência e não fará qualquer discriminação de raça, cor, gênero ou religião.
             </p>
         </div>
