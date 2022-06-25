a31481cbbfa6e7ef991e0af2c167b2e39668b773
Marcos Lohmann <marcos.lohmann@gmail.com>
2022-06-24 22:50:37 -0300

    Update nossos-projetos.php

--- src/view/nossos-projetos.php
+++ src/view/nossos-projetos.php
@@ -9,33 +9,26 @@ require_once "../view/header.php"
     <div class="container">
         <h1 class="text-center">Conheça um pouco mais sobre nossos projetos</h1>
 
-        <div class="d-flex">
-            <p class="text-start">Praça solidária</p>
-            <br>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, itaque nemo, autem doloribus laudantium rerum esse, velit perferendis iure ipsa ducimus officiis sunt explicabo. Tenetur qui officiis laborum eaque voluptate!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, itaque nemo, autem doloribus laudantium rerum esse, velit perferendis iure ipsa ducimus officiis sunt explicabo. Tenetur qui officiis laborum eaque voluptate!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, itaque nemo, autem doloribus laudantium rerum esse, velit perferendis iure ipsa ducimus officiis sunt explicabo. Tenetur qui officiis laborum eaque voluptate!
-            </p>
-            <img width="200rem" height="200rem" src="../public/images/projeto-praca-solidaria.svg" alt="">
+
+        <div class="d-flex mt-4">
+            <img width="200rem" height="200rem" src="../public/images/camp-vakinha.png" alt="imagem com qrcode para doações">
+            <p class="text-start">Projeto Solidário sem FOME</p>
+            <p>Somos o “Projeto Solidário sem FOME”! Somos um grupo multidisciplinar para INCLUSÃO SOCIAL!<br>
+Atuamos diretamente com moradores em situação de rua há cerca de 20 anos. Estamos na região do Morumbi e atendemos pessoas de toda a cidade de São Paulo.
+Servimos mais de 150 refeições e café da manhã por dia. Nos mantemos através de doações e principalmente pela venda de nossos Pães.<br>
+Com a pandemia de Covid-19 a situação de diversas pessoas piorou. Nosso “Projeto Solidário Sem FOME” também virou “Praça Solidária”! O isolamento social levou nossa Ação para a Praça. Passamos a servir refeições e organizar doações diretamente na praça para todas as pessoas que necessitam de ajuda.
+A demanda aumenta a cada dia!<br>
+Precisamos de novos equipamentos, principalmente uma “amassadeira” (batedeira industrial) e um forno industrial para podermos aumentar a produção de Pães e, consequentemente, atendermos a demanda.nossos Produtos são totalmente Artesanal..
+Para continuarmos nossa MISSÃO precisamos de sua ajuda.<br>
+Doe através de nossa “vaquinha” virtual.<br>
+DOE!!!</p>
         </div>
 
         <div class="d-flex mt-4">
-            <img width="200rem" height="200rem" src="../public/images/pao-solidario.svg" alt="foto de um pão caseiro">
-            <p class="text-start">Pão solidário</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, itaque nemo, autem doloribus laudantium rerum esse, velit perferendis iure ipsa ducimus officiis sunt explicabo. Tenetur qui officiis laborum eaque voluptate!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, itaque nemo, autem doloribus laudantium rerum esse, velit perferendis iure ipsa ducimus officiis sunt explicabo. Tenetur qui officiis laborum eaque voluptate!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, itaque nemo, autem doloribus laudantium rerum esse, velit perferendis iure ipsa ducimus officiis sunt explicabo. Tenetur qui officiis laborum eaque voluptate!
-            </p>
-        </div>
-
-        <div class="d-flex mt-4">
-            <p class="text-start">Arrecadação de ração para cachorros</p>
+            <p class="text-start">P&atilde;o Solid&aacute;rio</p>
             <br>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, itaque nemo, autem doloribus laudantium rerum esse, velit perferendis iure ipsa ducimus officiis sunt explicabo. Tenetur qui officiis laborum eaque voluptate!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, itaque nemo, autem doloribus laudantium rerum esse, velit perferendis iure ipsa ducimus officiis sunt explicabo. Tenetur qui officiis laborum eaque voluptate!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, itaque nemo, autem doloribus laudantium rerum esse, velit perferendis iure ipsa ducimus officiis sunt explicabo. Tenetur qui officiis laborum eaque voluptate!
-            </p>
-            <img width="200rem" height="200rem" src="../public/images/cachoro-com-cobertas.svg" alt="">
+            <p>Pães Solidarios quem ainda não provou é só encomendar ou levar para sua casa ou seu Condominio e com isso vc estará ajudando nosso Projeto Solidario sem fome. <a href='https://wa.me/5511981923972' target='_blank'>faça sua Doação</a></p>
+            <img width="200rem" height="200rem" src="../public/images/camp-paosol.png" alt="">
         </div>
     </div>
 
