b3289de6cfaa751a718560c1e35e98f26a4f001f
Marcos Lohmann <marcos.lohmann@gmail.com>
2022-06-24 23:07:49 -0300

    Update nossos-projetos.php

--- src/view/nossos-projetos.php
+++ src/view/nossos-projetos.php
@@ -12,13 +12,10 @@ require_once "../view/header.php"
 
         <div class="container d-flex mt-4">
             <img width="300rem" height="300rem" src="../public/images/camp-vakinha.PNG" alt="imagem com qrcode para doações" class='float-start'>
-            <div><h3>Projeto Solidário sem FOME</h3>
+            <div class='p-3'><h3>Projeto Solidário sem FOME</h3>
             <p>Somos o “Projeto Solidário sem FOME”! Somos um grupo multidisciplinar para INCLUSÃO SOCIAL!<br>
-Atuamos diretamente com moradores em situação de rua há cerca de 20 anos. Estamos na região do Morumbi e atendemos pessoas de toda a cidade de São Paulo.
-Servimos mais de 150 refeições e café da manhã por dia. Nos mantemos através de doações e principalmente pela venda de nossos Pães.<br>
-Com a pandemia de Covid-19 a situação de diversas pessoas piorou. Nosso “Projeto Solidário Sem FOME” também virou “Praça Solidária”! O isolamento social levou nossa Ação para a Praça. Passamos a servir refeições e organizar doações diretamente na praça para todas as pessoas que necessitam de ajuda.
+Com a pandemia de Covid-19 a situação de diversas pessoas piorou. Nosso “Projeto Solidário Sem FOME” também virou “Praça Solidária”! O isolamento social levou nossa ação para a Praça. Passamos a servir refeições e organizar doações diretamente na praça para todas as pessoas que necessitam de ajuda.
 A demanda aumenta a cada dia!<br>
-Precisamos de novos equipamentos, principalmente uma “amassadeira” (batedeira industrial) e um forno industrial para podermos aumentar a produção de Pães e, consequentemente, atendermos a demanda.nossos Produtos são totalmente Artesanal..
 Para continuarmos nossa MISSÃO precisamos de sua ajuda.<br>
 Doe através de nossa “vaquinha” virtual.<br>
 DOE!!!</p></div>
@@ -26,8 +23,12 @@ DOE!!!</p></div>
 
         <div class="container d-flex mt-4">
             <img width="300rem" height="300rem" src="../public/images/camp-paosol.PNG" alt="imagem de pães" class='float-end'>
-            <div><h3>P&atilde;o Solid&aacute;rio</h3>
-            <p>Pães Solidarios quem ainda não provou é só encomendar ou levar para sua casa ou seu Condominio e com isso vc estará ajudando nosso Projeto Solidario sem fome. <a href='https://wa.me/5511981923972' target='_blank'>faça sua Doação</a></p>
+            <div class='p-3'><h3>P&atilde;o Solid&aacute;rio</h3>
+            <p>Atuamos diretamente com moradores em situação de rua há cerca de 20 anos. Estamos na região do Morumbi e atendemos pessoas de toda a cidade de São Paulo.
+Servimos mais de 150 refeições e café da manhã por dia. Nos mantemos através de doações e principalmente pela venda de nossos Pães.</p>
+                <p>
+Precisamos de novos equipamentos, principalmente uma “amassadeira” (batedeira industrial) e um forno industrial para podermos aumentar a produção de pães e, consequentemente, atendermos a demanda. Nossos produtos são totalmente Artesanais</p>
+            <p>Pães Solidarios quem ainda não provou é só encomendar ou levar para sua casa ou seu condominio e com isso você estará ajudando nosso Projeto Solidario sem fome. <a href='https://wa.me/5511981923972' target='_blank'>Faça sua doação!</a></p>
                 </div>
         </div>
     </div>
